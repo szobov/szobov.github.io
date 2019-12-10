d79f6ea72fc2c43ca990bc3d5d61a6558fe5980f
Sergei Zobov <sergey@szobov.ru>
2019-12-10 12:03:07 +0300

    bin/check-links.sh: fix

--- bin/check-links.sh
+++ bin/check-links.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # telegram and linkedin are blocked in Russia
-htmlproofer --url-ignore https://t.me/szobov,https://www.linkedin.com/in/szobovdev ${1}
+LANG="C.UTF-8" LC_ALL="C.UTF-8" htmlproofer --url-ignore https://t.me/szobov,https://www.linkedin.com/in/szobovdev ${1}
